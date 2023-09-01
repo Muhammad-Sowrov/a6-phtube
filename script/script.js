3a9faa1895ea273e87fa68512b71c9a9595f7c08
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-09-01 12:20:42 +0600

    fetching in videos

--- script/script.js
+++ script/script.js
@@ -7,16 +7,18 @@ const handleCategory = async() =>{
         // console.log(category);
         const div = document.createElement("div");
         div.innerHTML = `
-        <a class="tab">${category.category}</a>
+        <button onClick = "handleVideos('${category.category_id}')" class="btn"><a class="tab">${category.category}</a></button>
         `;
         tabContainer.appendChild(div);
 
-    })
-    
-
-    console.log(data.data);
+    });
+    // console.log(data.data);
+};
+const handleVideos = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = response.json();
+    console.log(data);
 }
-
 
 
 handleCategory();
