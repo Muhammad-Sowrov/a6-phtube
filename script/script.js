0d32fcd5fde99b6d785bff1f136bef21802b5545
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-09-01 21:03:37 +0600

    responsive and blog done

--- script/script.js
+++ script/script.js
@@ -4,12 +4,15 @@ const handleCategory = async() =>{
 
     const tabContainer = document.getElementById("tab-container");
     const videosCategory = data.data;
+    
     videosCategory.forEach((category) => {
         // console.log(category);
         const div = document.createElement("div");
         div.innerHTML = `
-        <button onClick = "handleVideos('${category.category_id}')" class="btn"><a class="tab">${category.category}</a></button>
+        <button onClick = "handleVideos('${category.category_id}')" class="btn btn-ghost"><a class="tab">${category.category}</a></button>
         `;
+        
+
         tabContainer.appendChild(div);
 
     });
@@ -24,30 +27,43 @@ const handleVideos = async (categoryId) => {
         console.log(videos);
         const div = document.createElement("div");
         div.innerHTML = `
-        <div class="card w-full bg-base-100 shadow-xl">
+        <div class="card w-full bg-base-100 shadow-xl font-inter">
             <figure class="px-10 pt-10">
-              <img src="${videos.thumbnail}" alt="product" class="rounded-lg w-96" />
+              <img src="${videos?.thumbnail}" alt="product" class="rounded-lg w-full h-24" />
             </figure>
             <div class="items-center flex gap-4">
               <img
-                src="${videos.authors[0].profile_picture}"
+                src="${videos?.authors[0]?.profile_picture}"
                 alt="product"
                 class="rounded-full w-5"
               />
-              <h2 class="">
-                ${videos.title}
+              <h2 class="text-base font-bold">
+                ${videos?.title}
               </h2>
             </div>
             <div class="flex gap-4 items-center pl-10">
-              <p>${videos.authors}</p>
-              <i class="fa-solid fa-check text-blue-500"></i>
+              <p class="text-sm font-normal">${videos?.authors[0]?.profile_name}</p>
+
+
+              <i>${videos?.authors[0]?.verified}</p>
+
+
             </div>
-            <p class="pl-10">Views: 77k</p>
+            <p class="pl-10 text-sm font-normal">${videos?.others?.views} views</p>
           </div>
         `;
         videoContainer.appendChild(div);
     });
 };
+
+
+
+
+const blog = document.getElementById('handleBlog');
+const handleBlog = () =>{
+  window.open("blog.html", "_blank");
+}
+
 
 
 handleCategory();
